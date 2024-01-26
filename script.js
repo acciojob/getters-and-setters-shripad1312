4811ec03f575a6138cf681acf58c202ecb36728d
shripad1312 <96540872+shripad1312@users.noreply.github.com>
2024-01-26 12:51:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 //complete this code
-class Person {}
+class Person {
+	person(name,age){
+		this.name=name;
+		this.age=age;
+	}
 
-class Student extends Person {}
+	get name(){
+		return this.name;
+	}
+	get age(){
+		return this.age
+	}
+	set name(name){
+		this.name=name;
+	}
+	set age(age){
+		this.age=age;
+	}
+}
 
-class Teacher extends Person {}
+class Student extends Person {
+	Student(name,age){
+		super(name,age);
+	}
+	study(){
+		console.log(`${super.name} is studying `);
+	}
+}
+
+class Teacher extends Person {
+	Teacher(name,age){
+		super(name,age);
+	}
+
+	teach(){
+		console.log(`${super.name} is teaching `);
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
